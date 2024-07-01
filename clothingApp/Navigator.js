4a93e172fc32194ff1f7dec95274b2904d945809
theboyGolden <preciousstick15@gmail.com>
2024-07-01 23:56:50 +0000

    Working

--- clothingApp/Navigator.js
+++ clothingApp/Navigator.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { Image, View, Text, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './Screens/HomeScreen';
 import CartScreen from './Screens/CartScreen';
+import Icon from 'react-native-vector-icons/Ionicons'; // Example icon library
 
 const Drawer = createDrawerNavigator();
 
+const CustomHeader = ({ navigation }) => {
+  return {
+    headerTitle: () => (
+      <View style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: 70 }}>
+        <Image source={require('./assets/Logo.png')} style={{marginRight: 30 }}/>
+        <View style={{ flexDirection: 'row', marginLeft: 50,}}>
+            <Image source={require('./assets/Search.png')} style={{ marginRight: 20}}/>
+            <Image source={require('./assets/shoppingBag.png')} style={{ }}/>
+        </View>
+      </View>
+    ),
+  };
+};
+
 function DrawerGroup() {
   return (
-    <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Navigator screenOptions={({ navigation }) => CustomHeader({ navigation })}>
       <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Cart" component={CartScreen} />
+      <Drawer.Screen name="Cart" component={CartScreen} options={{ headerShown: false }} />
     </Drawer.Navigator>
   );
 }
--- clothingApp/Screens/HomeScreen.js
+++ clothingApp/Screens/HomeScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, View, Text, Image } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView style={{flex:1, paddingLeft: 10, backgroundColor: '#fff'}}>
+        <View style={{flexDirection: 'row', marginTop: 10}}>
+            <View style={{paddingTop: 10}}><Text style={{fontSize: 20, fontWeight: '500', fontFamily: 'Verdana', paddingRight: 130, }}>OUR STORY</Text> </View>
+            <View style={{marginRight:20, padding: 10, backgroundColor: '#ededed', borderRadius: 30 }}><Image source={require('../assets/thumbnail.png')} style={{height:25, width: 25}}/> </View>
+            <View style={{padding: 8, backgroundColor: '#ededed', borderRadius: 30 }}><Image source={require('../assets/filter.png')} style={{height:30, width: 30}}/></View>
+        </View>
     </SafeAreaView>
   );
 }
